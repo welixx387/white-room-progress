8692ac766394d087bfb22b366256eae2f3d251e9
welixx387 <gulmetovsamil835@gmail.com>
2026-07-05 17:28:16 +0300

    Create tailwind.config.js

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  darkMode: 'class',
+  content: [
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        brand: {
+          light: '#60A5FA',
+          DEFAULT: '#3B82F6',
+          dark: '#1D4ED8',
+        }
+      }
+    },
+  },
+  plugins: [],
+};
